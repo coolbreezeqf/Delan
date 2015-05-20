8b1771a869ad04369c06fe95e8c2ecf0170513ac
colbrze <colbrze430@gmail.com>
2015-05-20 17:08:06 +0800

    commit test

---
+++ Delan/Delan/Public.h
@@ -0,0 +1,67 @@
+//
+//  Public.h
+//  Delan
+//
+//  Created by qf on 15/5/20.
+//  Copyright (c) 2015年 wdb. All rights reserved.
+//
+
+#ifndef Delan_Public_h
+#define Delan_Public_h
+
+#define kMainScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kMainScreenWidth  [UIScreen mainScreen].bounds.size.width
+
+#define RGBCOLOR(r,g,b) [UIColor colorWithRed:r/256.0 green:g/256.0 blue:b/256.0 alpha:1]
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:r/256.0 green:g/256.0 blue:b/256.0 alpha:a]
+
+#define kSystemVersion [[[UIDevice currentDevice] systemVersion] floatValue]
+#define kSofterViewsion  [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
+
+#ifdef DEBUG
+#define MLOG(...)  printf("\n\t<%s line%d>\n%s\n", __FUNCTION__,__LINE__,[[NSString stringWithFormat:__VA_ARGS__] UTF8String])
+
+#else
+#define MLOG(...)
+#define NSLog(...) {}
+#endif
+
+#define kFont10 [UIFont systemFontOfSize:10]
+#define kFont11 [UIFont systemFontOfSize:11]
+#define kFont12 [UIFont systemFontOfSize:12]
+#define kFont13 [UIFont systemFontOfSize:13]
+#define kFont14 [UIFont systemFontOfSize:14]
+#define kFont15 [UIFont systemFontOfSize:15]
+#define kFont16 [UIFont systemFontOfSize:16]
+#define kFont17 [UIFont systemFontOfSize:17]
+#define kFont18 [UIFont systemFontOfSize:18]
+#define kFont19 [UIFont systemFontOfSize:19]
+#define kFont20 [UIFont systemFontOfSize:20]
+#define kFont21 [UIFont systemFontOfSize:21]
+#define kFont22 [UIFont systemFontOfSize:22]
+#define kFont23 [UIFont systemFontOfSize:23]
+#define kFont24 [UIFont systemFontOfSize:24]
+#define kFont25 [UIFont systemFontOfSize:25]
+#define kFont26 [UIFont systemFontOfSize:26]
+#define kFont27 [UIFont systemFontOfSize:27]
+#define kFont28 [UIFont systemFontOfSize:28]
+#define kFont29 [UIFont systemFontOfSize:29]
+#define kFont30 [UIFont systemFontOfSize:30]
+
+#define kFontBold13 [UIFont fontWithName:@"Helvetica-Bold" size:13]
+#define kFontBold15 [UIFont fontWithName:@"Helvetica-Bold" size:15]
+#define kFontBold17 [UIFont fontWithName:@"Helvetica-Bold" size:17]
+#define kFontBold18 [UIFont fontWithName:@"Helvetica-Bold" size:18]
+#define kFontBold19 [UIFont fontWithName:@"Helvetica-Bold" size:19]
+#define kFontBold20 [UIFont fontWithName:@"Helvetica-Bold" size:20]
+#define kFontBold21 [UIFont fontWithName:@"Helvetica-Bold" size:21]
+#define kFontBold22 [UIFont fontWithName:@"Helvetica-Bold" size:22]
+#define kFontBold23 [UIFont fontWithName:@"Helvetica-Bold" size:23]
+#define kFontBold24 [UIFont fontWithName:@"Helvetica-Bold" size:24]
+#define kFontBold25 [UIFont fontWithName:@"Helvetica-Bold" size:25]
+
+#define kNSUDefaultSaveVauleAndKey(value,key) [[NSUserDefaults standardUserDefaults] setObject:value forKey:key]
+#define kNSUDefaultReadKey(key) [[NSUserDefaults standardUserDefaults] valueForKey:key]
+
+
+#endif
