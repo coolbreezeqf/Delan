5b8be02656b0d6e17af47b0ffcfedb28f2df4812
colbrze <colbrze430@gmail.com>
2015-05-20 18:12:13 +0800

    try

---
+++ Delan/Delan/MoreTab/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  Delan
+//
+//  Created by qf on 15/5/20.
+//  Copyright (c) 2015年 wdb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
