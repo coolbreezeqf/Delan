5794d921bfe423a9da4590ccd3b751a1db39ae6e
lht <877674727@qq.com>
2015-06-05 18:02:26 +0800

    //123 添加了viewcontroller

---
+++ Delan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delan
+//
+//  Created by 广有射怪鸟事 on 15/6/5.
+//  Copyright (c) 2015年 wdb. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ViewController : BaseViewController
+
+@end
